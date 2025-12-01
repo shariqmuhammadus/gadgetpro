52a18fb13e6b6cde9a59b8e409372e82c51fbc9d
Muhammad Shariq <muhammadshariq@Muhammads-MacBook-Pro.local>
2025-12-01 00:33:15 -0600

    Feat: Added states

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -10,8 +10,11 @@ import {
   View,
 } from "react-native";
 import { CATEGORIES } from "../../assets/categories";
+import { useCartStore } from "../store/cart-store";
 
 export const ListHeader = () => {
+  const { getItemCount } = useCartStore();
+
   return (
     <View style={[styles.headerContainer]}>
       <View style={styles.headerTop}>
@@ -36,7 +39,7 @@ export const ListHeader = () => {
                     style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
                   />
                   <View style={styles.badgeContainer}>
-                    <Text style={styles.badgeText}>{3}</Text>
+                    <Text style={styles.badgeText}>{getItemCount()}</Text>
                   </View>
                 </View>
               )}
